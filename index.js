2d810d53936bd158e2f408b09439ecad2206d3f2
Paul Buscano <paul.buscano@thethurchco.com>
2025-04-14 22:14:02 +0800

    video id only

--- index.js
+++ index.js
@@ -7,41 +7,16 @@ const app = express();
 var cors = require('cors');
 app.use(cors());
 
-// Load cookies from cookies.json file
-let cookiesString = '';
-try {
-  const cookiesPath = path.join(__dirname, 'cookies.json');
-  const cookiesJson = JSON.parse(fs.readFileSync(cookiesPath, 'utf8'));
-
-  // Convert cookies JSON to string format ytdl-core expects
-  if (Array.isArray(cookiesJson)) {
-    cookiesString = cookiesJson
-      .map((cookie) => `${cookie.name}=${cookie.value}`)
-      .join('; ');
-  } else {
-    // Handle if cookies.json has a different structure
-    cookiesString = JSON.stringify(cookiesJson);
-  }
-  console.log('Cookies loaded successfully');
-} catch (error) {
-  console.error('Error loading cookies:', error.message);
-}
-
 app.get('/download', async (req, res) => {
   try {
-    const url = req.query.url;
-    const videoId = await ytdl.getURLVideoID(url);
+    const videoId = req.query.videoId;
 
-    // Pass cookies in the request options
-    const requestOptions = {
-      requestOptions: {
-        headers: {
-          Cookie: cookiesString,
-        },
-      },
-    };
+    const agent = ytdl.createAgent(JSON.parse(fs.readFileSync('cookies.json')));
 
-    const metaInfo = await ytdl.getInfo(url, requestOptions);
+    const metaInfo = await ytdl.getInfo(
+      `http://www.youtube.com/watch?v=${videoId}`,
+      { agent }
+    );
     let data = {
       url: 'https://www.youtube.com/embed/' + videoId,
       info: metaInfo.formats,
