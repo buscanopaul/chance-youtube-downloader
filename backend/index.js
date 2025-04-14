21e0253209ee727f844372c31235eefe0fb0c260
Paul Buscano <paul.buscano@thethurchco.com>
2025-04-14 20:19:23 +0800

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const ytdl = require('ytdl-core')
+const express = require('express');
+const ytdl = require('@distube/ytdl-core');
 
-const app = express()
-var cors = require("cors");
+const app = express();
+var cors = require('cors');
 app.use(cors());
 
 app.get('/download', async (req, res) => {
-    try {
-        const url = req.query.url
-        const videoId = await ytdl.getURLVideoID(url)
-        const metaInfo = await ytdl.getInfo(url)
-        let data = {
-            url: 'https://www.youtube.com/embed/'+videoId,
-            info: metaInfo.formats
-        }
-        return res.send(data)
-    } catch(error) {
-        return res.status(500)
-    }
-})
+  try {
+    const url = req.query.url;
+    const videoId = await ytdl.getURLVideoID(url);
+    const metaInfo = await ytdl.getInfo(url);
+    let data = {
+      url: 'https://www.youtube.com/embed/' + videoId,
+      info: metaInfo.formats,
+    };
+    return res.send(data);
+  } catch (error) {
+    return res.status(500);
+  }
+});
 
 app.listen(4000, () => {
-    console.log(`Server is running on PORT: 4000`)
-})
+  console.log(`Server is running on PORT: 4000`);
+});
