f18c2ae6db775787e87719be7d826b0177a1b64b
Paul Buscano <paul.buscano@thethurchco.com>
2025-04-14 21:43:30 +0800

    revert

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
 const express = require('express');
-const youtubeDl = require('youtube-dl-exec');
+const ytdl = require('@distube/ytdl-core');
 
 const app = express();
-app.use(require('cors')());
+var cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send('YouTube to MP4 API is running');
-});
+app.use(cors());
 
 app.get('/download', async (req, res) => {
   try {
     const url = req.query.url;
-    if (!url) {
-      return res.status(400).send({ error: 'URL is required' });
-    }
-
-    // Get video info using youtube-dl
-    const output = await youtubeDl(url, {
-      dumpSingleJson: true,
-      noWarnings: true,
-      noCallHome: true,
-      preferFreeFormats: true,
-      youtubeSkipDashManifest: true,
-    });
-
-    const videoId = url.includes('v=')
-      ? url.split('v=')[1].split('&')[0]
-      : output.id;
-
+    const videoId = await ytdl.getURLVideoID(url);
+    const metaInfo = await ytdl.getInfo(url);
     let data = {
       url: 'https://www.youtube.com/embed/' + videoId,
-      info: output.formats,
+      info: metaInfo.formats,
     };
-
     return res.send(data);
   } catch (error) {
-    console.error('Error:', error);
-    return res.status(500).send({ error: error.message });
+    return res.status(500);
   }
 });
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+app.listen(4000, () => {
+  console.log(`Server is running on PORT: 4000`);
 });
